dca2b3cc14d8c118cb79613b8700b6499030c33a
kailoan <467364@student.saxion.nl>
2019-09-26 14:58:48 +0300

    Added dymamic coloration to the login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,7 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['login.page.scss']
 })
 export class LoginPage {
+  user = {
+    username: '',
+    password: '',
+    remembered: null
+  };
+  isFormValid = false;
 
-  constructor() {}
+  constructor() { 
+  }
 
+  checkCurrentForm() {
+    let userObj = this.user;
+    //console.log(userObj.username + " " + userObj.username.length)
+    if (userObj.username.length == 8 && (userObj.password.length >= 8 && userObj.password.length <= 12 )) {
+      this.isFormValid = true;
+    } else {
+      this.isFormValid = false;
+    }
+  }
+
+  submitInputs() {
+    let userObj = this.user;
+    console.log(userObj);
+  }
+
+  isDataValid(username, password) {
+    if (username.valid && password.valid) {
+      return true;
+    }
+    return false;
+  }
 }
