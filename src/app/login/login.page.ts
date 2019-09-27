fc42a9fca59a53d98e71ff3b93897eeff16f6e96
kailoan <467364@student.saxion.nl>
2019-09-27 12:38:03 +0300

    Added proper dynamic highlighting

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss']
 })
+
 export class LoginPage {
   user = {
     username: '',
@@ -12,29 +13,45 @@ export class LoginPage {
     remembered: null
   };
   isFormValid = false;
+  isUsernameValid = false;
+  isPasswordValid = false;
 
-  constructor() { 
+  constructor() {
   }
 
   checkCurrentForm() {
     let userObj = this.user;
-    //console.log(userObj.username + " " + userObj.username.length)
-    if (userObj.username.length == 8 && (userObj.password.length >= 8 && userObj.password.length <= 12 )) {
+    this.isUsernameValid = this.usernameValidation(userObj.username);
+    this.isPasswordValid = this.passwordValidation(userObj.password);
+    if (this.isUsernameValid && this.isPasswordValid) {
       this.isFormValid = true;
     } else {
       this.isFormValid = false;
     }
   }
 
-  submitInputs() {
-    let userObj = this.user;
-    console.log(userObj);
+  usernameValidation(username) {
+    if (username.length == 8) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  isDataValid(username, password) {
-    if (username.valid && password.valid) {
+  passwordValidation(password) {
+    if (password.length >= 8 && password.length <= 12) {
       return true;
+    } else {
+      return false;
     }
-    return false;
+  }
+
+  submitInputs() {
+    this.checkCurrentForm();
+    if (this.isFormValid) {
+      alert("form is valid");
+    } else {
+      alert("form is invalid")
+    }
   }
 }
