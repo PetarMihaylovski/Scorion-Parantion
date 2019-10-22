ad2deb6c74cc29066df4393ed19c521c2d034091
kailoan <467364@student.saxion.nl>
2019-10-22 23:05:20 +0300

    Added basic layout and functionality for giving feedback

--- src/app/lecturer-home-page/give-feedback/give-feedback.page.ts
+++ src/app/lecturer-home-page/give-feedback/give-feedback.page.ts
@@ -7,9 +7,66 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./give-feedback.page.scss'],
 })
 export class GiveFeedbackPage implements OnInit {
-  areWeOnStudentsPage = true;
-
   constructor(private navCtrl: NavController) { }
+
+  isFormValid = false;
+
+  feedback = {
+    student: '',
+    context: '',
+    description: ''
+  };
+  isStudentValid = false;
+  isContextValid = false;
+  isDescriptionValid = false;
+  isRecording = false;
+
+  checkCurrentForm() {
+    let feedbackObj = this.feedback;
+    this.isStudentValid = this.studentValidation(feedbackObj.student);
+    this.isContextValid = this.contextValidation(feedbackObj.context);
+    this.isDescriptionValid = this.descriptionValidation(feedbackObj.description);
+    if (this.isStudentValid && this.isContextValid && this.isDescriptionValid) {
+      this.isFormValid = true;
+    } else {
+      this.isFormValid = false;
+    }
+  }
+
+  studentValidation(student) {
+    if (student.length >= 7 && student.length <= 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  contextValidation(context) {
+    if (context.length >= 7 && context.length <= 12) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  descriptionValidation(description) {
+    if (description.length >= 7 && description.length <= 12) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  submitFeedback() {
+  }
+
+  navigateToHomeScreen() {
+    this.navCtrl.navigateForward('/lecturer-home');
+  }
+
+  toggleRecording() {
+    this.isRecording = !this.isRecording;
+  }
 
   ngOnInit() {}
   
